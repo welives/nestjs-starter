ef71326d2e7541de2830d1073dba458a7583a77f
Jandan <lutao1096@gmail.com>
2023-11-01 22:15:52 +0800

    日志

--- apps/app/src/app.module.ts
+++ apps/app/src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import * as Joi from 'joi'
+import * as winston from 'winston'
+import { WinstonModule } from 'nest-winston'
+import 'winston-daily-rotate-file'
 
 @Module({
   imports: [
@@ -13,6 +16,25 @@ import * as Joi from 'joi'
         APP_HOST: Joi.string().required(),
         APP_PORT: Joi.number().default(3000),
       }),
+    }),
+    WinstonModule.forRoot({
+      exitOnError: false, // 出现 uncaughtException 时是否 process.exit
+      transports: [
+        new winston.transports.DailyRotateFile({
+          silent: process.env.NODE_ENV !== 'production',
+          dirname: 'logs/app', // 日志保存的目录
+          filename: '%DATE%.log', // 日志名称，占位符 %DATE% 取值为 datePattern 值
+          datePattern: 'YYYY-MM-DD', // 日志轮换的频率，此处表示每天
+          zippedArchive: true, // 是否通过压缩的方式归档被轮换的日志文件
+          maxSize: '20m', // 设置日志文件的最大大小，m 表示 mb
+          maxFiles: '14d', // 保留日志文件的最大天数，此处表示自动删除超过 14 天的日志文件
+          // 记录时添加时间戳信息
+          format: winston.format.combine(
+            winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            winston.format.json()
+          ),
+        }),
+      ],
     }),
   ],
   controllers: [AppController],
